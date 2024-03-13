dab7ccfc4515c8055d2fbb6b8f77a97bf1c78613
Service Web <sw@gssmail.com>
2024-03-13 15:25:26 -0500

    Commit changes in C:\Websites\Git\emarlow\5286\GCG_5286_MVE_TKT_SU.sql

---
+++ GCG_5286_MVE_TKT_SU.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5286_MVE_TKT_SU
+ * ========================================== */
+CREATE TABLE "GCG_5286_MVE_TKT_SU"(
+ "TRMNL" CHAR(3) NOT NULL ,
+ "COMMENTS" LONGVARCHAR NOT NULL ,
+ "PRINTER" CHAR(30),
+ "SCRAPPRINTER" CHAR(100));
+
+
